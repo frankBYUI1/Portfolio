1a4a97151977b702da0c1f7f649c9ef3cc199bbb
Francesco Lezano <lez18002@byui.edu>
2023-09-17 23:09:08 -0600

    first_assignment

--- intro.py
+++ intro.py
@@ -1,1 +1,5 @@
+"""
+Using Visual Studio Code, write a very simple "Hello World" program in any programming language that you already know 
+(e.g. Python, C++, C#, or anything else). The code should be placed in its own folder.
+"""
 print('hello world')
